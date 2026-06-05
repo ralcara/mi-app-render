f017cdcab9735b14c0bf16cb5ffd64051c2e200a
rocio <ralcara908@g.educaand.es>
2026-06-05 11:03:03 +0200

    agrego tests unitarios, integracion y GitHub Actions

---
+++ app.js
@@ -0,0 +1,13 @@
+﻿const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Version 1 desplegada con Render</h1>');
+});
+
+if (require.main === module) {
+  app.listen(PORT, () => console.log('Servidor en puerto ' + PORT));
+}
+
+module.exports = app;
---
+++ sum.test.js
@@ -0,0 +1,9 @@
+﻿const sum = require('./sum');
+
+test('suma 2 + 3 debe dar 5', () => {
+  expect(sum(2, 3)).toBe(5);
+});
+
+test('suma con numero negativo', () => {
+  expect(sum(-1, 4)).toBe(3);
+});
